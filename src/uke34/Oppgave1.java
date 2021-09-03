499f84e5651e19af138be32e1ffa67bf3dc6c273
rebeccaweibyfriden <rebecca.friden@gmail.com>
2021-09-03 19:56:18 +0200

    Oppgave1

--- src/uke34/Oppgave1.java
+++ src/uke34/Oppgave1.java
@@ -1,4 +1,36 @@
 package uke34;
 
 public class Oppgave1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int[] a = {8, 4, 17, 10, 6, 20, 1, 11, 15, 3, 18, 9, 2, 7, 19};
+
+        int min_index = min(a);
+        System.out.println(min_index);
+        System.out.println(a[min_index]);
+
+    }
+
+    static int min(int[] a) {
+        //initialiserer med førtste element.
+        int min_value = a[0];
+        int min_index = 0;
+
+        for(int i = 1; i<a.length; i++){
+            int value = a[i];
+            int index = i;
+
+            //sjekk om verdien er mindre enn den vi har sett tidligere.
+            if (value < min_value){
+                min_value = value;
+                min_index = index;
+
+            }
+        }
+
+        //returnerer indexen til den minste verdien. 
+        return min_index;
+    }
+
 }
---
+++ src/uke34/Oppgave2.java
@@ -0,0 +1,10 @@
+package uke34;
+
+public class Oppgave2 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+
+}
+
